c3f3bc67f79efb1b001df91865766d7bd8ad88a9
Cloumus30 <62823377+Cloumus30@users.noreply.github.com>
2021-01-20 22:29:57 +0700

    misah file dana dan nisfa
    misahkan program nisfa dan Dana

--- script.js
+++ script.js
@@ -1,14 +1,7 @@
-// kondisi run program
-// true: lanjut terus
-// false: program berhenti
-// let runProgram = true;
-// // While loop untuk mengulang program terus menerus sampai user tidak ingin berhitung lagi
-// while (runProgram) {
 //input operator oleh user
 let inputOperasi = prompt('Pilih Operator' + '\n' + 'ex : +,-,x,:');
 // Jika user tekan Cancel
 if (inputOperasi == null) {
-    // runProgram = false;
     alert('program selesai');
     fail;
 }
@@ -25,21 +18,6 @@ if ((inputOperasi == '+') || (inputOperasi == '-') || (inputOperasi == 'x') || (
     }
     // ubah angka1 jadi float
     angka1 = parseFloat(angka1);
-    // cek apakah angka1 bertipe angka atau bukan
-    // if (!angka1) {
-    //     alert("masukkan bukan angka")
-    //     // tanya user mau lanjut apa nggak
-    //     if (!confirm('Mau lanjut?')) {
-    //         // jika user nggk lanjut runprogram jadi false
-    //         runProgram = false;
-    //         alert('Program Selesai');
-    //         break;
-    //     }
-    //     // Jika user lanjut maka kembali ke awal 
-    //     else {
-    //         continue;
-    //     }
-    // }
 
     //input bilangan 2 oleh user
     let angka2 = prompt('Masukkan bilangan kedua');
@@ -51,21 +29,6 @@ if ((inputOperasi == '+') || (inputOperasi == '-') || (inputOperasi == 'x') || (
     }
     // ubah angka2 jadi float
     angka2 = parseFloat(angka2);
-    // cek apakah angka2 bertipe angka atau bukan
-    // if (!angka2) {
-    //     alert("masukkan bukan angka")
-    //     // tanya user mau lanjut apa nggak
-    //     if (!confirm('Mau lanjut?')) {
-    //         // jika user nggk lanjut runprogram jadi false
-    //         runProgram = false;
-    //         alert('Program Selesai');
-    //         break;
-    //     }
-    //     // Jika user lanjut maka kembali ke awal
-    //     else {
-    //         continue;
-    //     }
-    // }
 
     //pengondisian
     if (inputOperasi == '+') {
@@ -83,22 +46,11 @@ if ((inputOperasi == '+') || (inputOperasi == '-') || (inputOperasi == 'x') || (
     }
     alert('Hasil dari   ' + angka1 + inputOperasi + angka2 + '   adalah ' + hasil);
 
-    // tanya user mau lanjut atau tidak
-    // if (!confirm('Mau lanjut?')) {
-    //     // jika user nggk lanjut runprogram jadi false
-    //     runProgram = false;
-    //     alert('Program Selesai');
-    // }
-
     // Jika Operator Salah
 } else {
-    // tanya user lanjut atau tidak
+
     alert('Masukkan Operator Salah');
     alert('program Selesai')
-    // if (!confirm('Mau lanjut?')) {
-    //     // jika user nggk lanjut runProgram jadi false
-    //     runProgram = false;
-    //     alert('Program Selesai');
-    // }
+
 }
 // }
